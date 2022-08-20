873bedae0c3837cfaa9357b2fa8bc512ad5b1658
NatalliaDrozd <nat.dr000@gmail.com>
2022-08-20 21:40:14 +0300

    added new collectionCells

---
+++ EasyTrip/EasyTrip/Cells/CollectionCells/HotelsCell/HotelsCell.swift
@@ -0,0 +1,19 @@
+//
+//  HotelsCell.swift
+//  EasyTrip
+//
+//  Created by Natalia Drozd on 20.08.22.
+//
+
+import UIKit
+
+class HotelsCell: UICollectionViewCell {
+    static let key = "HotelsCell"
+    @IBOutlet weak var nameHotel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
