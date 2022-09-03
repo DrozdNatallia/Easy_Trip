6e47e85a3baf860a86eae1e624302601b8d3be83
NatalliaDrozd <nat.dr000@gmail.com>
2022-09-03 14:32:06 +0300

    add model Users

---
+++ EasyTrip/EasyTrip/Models/Users.swift
@@ -0,0 +1,19 @@
+//
+//  Users.swift
+//  EasyTrip
+//
+//  Created by Natalia Drozd on 3.09.22.
+//
+
+import Foundation
+
+struct Users {
+    var name: String?
+    var secondname: String?
+    var patronicum: String?
+  //  var city: String?
+ //   var sex: String?
+    var dateOfBirth: String?
+    var url: String?
+    
+}
